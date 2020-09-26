2b10816e5a92d7327bff3ba86817f214ab6d6bf5
Emma Paris <mayankpurohit2012@gmail.com>
2020-09-26 13:21:28 +0530

    [Mayank] Add Length of Line

--- Program.cs
+++ Program.cs
@@ -2,6 +2,19 @@
 
 namespace LineComparisonComputation
 {
+    class Line
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+
+        public double Length()
+        {
+            return Math.Sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1));
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
