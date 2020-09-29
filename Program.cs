ae15aeed5759541e5097077c71b87bec4b339128
Emma Paris <mayankpurohit2012@gmail.com>
2020-09-29 17:35:38 +0530

    [Mayank] Refactor Implemented Line and Point using classes

--- Program.cs
+++ Program.cs
@@ -2,16 +2,20 @@
 
 namespace LineComparisonComputation
 {
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
     class Line
     {
-        public int X1 { get; set; }
-        public int Y1 { get; set; }
-        public int X2 { get; set; }
-        public int Y2 { get; set; }
+        public Point P1 { get; set; }
+        public Point P2 { get; set; }
+
 
         public double Length()
         {
-            return Math.Sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1));
+            return Math.Sqrt((P2.X - P1.X) * (P2.X - P1.X) + (P2.Y - P1.Y) * (P2.Y - P1.Y));
         }
 
     }
@@ -21,8 +25,8 @@ namespace LineComparisonComputation
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
 
-            var line1 = new Line { X1 = 0, X2 = 0, Y1 = 1, Y2 = 1 };
-            var line2 = new Line { X1 = 0, X2 = 0, Y1 = -1, Y2 = -1 };
+            var line1 = new Line { P1 = new Point {X = 0, Y = 0 }, P2 = new Point {X=1, Y=1 } };
+            var line2 = new Line { P1 = new Point { X = 0, Y = 0 }, P2 = new Point { X = -1, Y = -1 } };
             Console.WriteLine(line1.Length().Equals(line2.Length()) ? "Lines are Equal" : (line1.Length() > line2.Length() ? "Line 1 is Greater than Line 2" : "Line 2 is Greater than Line1"));
             
 
